e76a4dab4078b4005963900525dbb3938ec56a28
Sheldon Christiansen <bigbrownguy724@gmail.com>
2023-05-17 13:55:28 -0600

    yup

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.use(express.json())
 
 app.use(express.static(__dirname + '/public'))
 
+app.post('/ma-post', (req,res) => {
+    res.send("yah posted")
+})
+
 app.listen(4000, () => {
     console.log("we are up on 4000");
 })
